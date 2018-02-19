591743c05a15f4ffda6942158eb033bb345a27bc
Khaick Oliveira Brito <khaickbrito@gmail.com>
2018-02-19 19:37:34 -0300

    Update ShorterDistance Comments

--- terceira_unidade/MenorDistancia/divideAndConquer/shorterDistance.js
+++ terceira_unidade/MenorDistancia/divideAndConquer/shorterDistance.js
@@ -11,26 +11,41 @@ module.exports = function (points) {
  * @param {Number} end Indice do fim da divisão
  */
 function shorterDistanceRecursive(points, begin, end) {
+    //Caso tenha-se chegado à um vetor com 2 ou menos valores
     if (end <= begin + 1)
+        //Para um vetor com 1 valor
         if (end == begin)
+            //Retorna-se o caso base:
+            //Distancia de 1 para ele mesmo é desconsiderada
             return {
                 distance: Infinity,
                 p1: points[begin],
                 p2: points[end]
             }
+        //Para um vetor com 2 valores
         else
+            //Calcula-se a distancia entre eles
+            //Retorna-se o valor encontrado e os respectivos pontos.
             return {
                 distance: calcDistance(points[end], points[begin]),
                 p1: points[begin],
                 p2: points[end]
             };
     else {
+        //Divide-se o vetor no meio
         midIndex = parseInt((begin + end) / 2);
+        //Recursivamente aplica-se a mesma função para cada lado do vetor bipartido
         left = shorterDistanceRecursive(points, begin, midIndex);
         right = shorterDistanceRecursive(points, midIndex + 1, end);
+        //Depois de computadas as distâncias através da recursividade
+        // calcula-se o menor valor entre a menor distância encontrada à Direita, à Esquerda
+        // e entre os valores centrais
         distance = Math.min(left.distance, right.distance, calcDistance(points[midIndex + 1], points[midIndex]));
+        //Caso a distancia à esquerda seja a menor, retorna-se Left que contem internamente os pontos associados
         if (distance == left.distance) return left;
+        //Mesmo comportamento para a Direita
         else if (distance == right.distance) return right;
+        //Mesmo comportamento para caso seja a distancia entre os elementos centrais
         else return { distance, p1: points[midIndex], p2: points[midIndex + 1] }
 
     }
